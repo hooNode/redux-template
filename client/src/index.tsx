f550e0c548633e42d7eb452022add82cec09c7fe
kth7024 <guinness5167@gmail.com>
2022-05-16 14:53:03 +0900

    redux setting clear

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -10,11 +10,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    {/* <Provider store={store}>
-      <PersistGate persistor={persistor}> */}
     <App />
-    {/* </PersistGate> */}
-    {/* </Provider> */}
   </React.StrictMode>
 );
 
